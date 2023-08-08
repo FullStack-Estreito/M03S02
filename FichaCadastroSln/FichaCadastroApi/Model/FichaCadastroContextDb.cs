c8f97e81b966a66dd2661be7287a940b34992f5b
RogeMattos <rogemattos.rm@gmail.com>
2023-08-08 19:50:21 -0300

    Criação Classe Db context

---
+++ FichaCadastroSln/FichaCadastroApi/Model/FichaCadastroContextDb.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FichaCadastroApi.Model
+{
+    public class FichaCadastroDbContext : DbContext
+    {
+        public FichaCadastroDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
