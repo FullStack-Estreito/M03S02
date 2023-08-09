1e76ca1b978e709a7cc5f8639b6bd1d36e068089
RogeMattos <rogemattos.rm@gmail.com>
2023-08-08 22:32:31 -0300

    Criação classe DetalheModel

--- FichaCadastroSln/FichaCadastroApi/Model/DetalheModel.cs
+++ FichaCadastroSln/FichaCadastroApi/Model/DetalheModel.cs
@@ -1,8 +1,13 @@
-﻿namespace FichaCadastroApi.Model
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FichaCadastroApi.Model
 {
     public class DetalheModel
     {
-
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
 
     }
 }
