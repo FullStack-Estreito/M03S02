9c67b185a113317b981593c74baa7bffbed3473e
RogeMattos <rogemattos.rm@gmail.com>
2023-08-08 21:46:28 -0300

    Criação da Classe Db context

---
+++ FichaCadastroSln/FichaCadastroApi/Model/FichaModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FichaCadastroApi.Model
+
+{
+    [Table("Ficha")]
+    public class FichaModel
+
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+    
+        [Column(TypeName = "VARCHAR"), Required, StringLength(250)]
+        public string Nome { get; set; }
+
+        [Column(TypeName = "VARCHAR"), Required, StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime DataNascimento { get; set; }
+
+        [Required]
+        public DateTime DataCadastro { get; set; }
+
+       
+
+    }
+
+}
